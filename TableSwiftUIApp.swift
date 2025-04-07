1453d97a9c786f4c3cb89f4941a94ba3b0911d92
Sam C <samcwebdev@gmail.com>
2025-04-07 12:11:45 -0500

    Update TableSwiftUIApp.swift

--- TableSwiftUIApp.swift
+++ TableSwiftUIApp.swift
@@ -1,17 +1,1 @@
-//
-//  TableSwiftUIApp.swift
-//  TableSwiftUI
-//
-//  Created by Sam Chutter on 4/2/25.
-//
 
-import SwiftUI
-
-@main
-struct TableSwiftUIApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
